40b763286b5b871eca34181345d903aca0e99bd5
Vittorio Rigamonti <rigazilla@gmail.com>
2016-11-23 10:59:17 +0100

    Example with constant hash and failures

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,44 @@
 // A basic sample application using Junction.
 
 #include <junction/ConcurrentMap_Grampa.h>
+#include <iostream>
 
-struct Foo {
+class Foo {
+  public:
+	Foo(int a) : a(a) {}
+  int a;
+};
+
+struct IntKeyTraits {
+    typedef int Key;
+    typedef typename turf::util::BestFit<int>::Unsigned Hash;
+    static const Key NullKey = Key(0);
+    static const Hash NullHash = Hash(0);
+    static Hash hash(int key) {
+         if (key % 2)
+          return 42;
+         else return 43;
+    }
 };
 
 void testMap() {
     // Create the map.
-    typedef junction::ConcurrentMap_Grampa<turf::u32, Foo*> ConcurrentMap;
+    typedef junction::ConcurrentMap_Grampa<turf::u32, Foo*, IntKeyTraits> ConcurrentMap;
     ConcurrentMap myMap;
 
     // Do a few operations.
-    myMap.assign(14, new Foo);
-    Foo* foo = myMap.get(14);
-    foo = myMap.exchange(14, new Foo);
-    delete foo;
-    foo = myMap.erase(14);
-    delete foo;
+    myMap.assign(14, new Foo(14));
+    myMap.assign(15, new Foo(15));
+    myMap.assign(16, new Foo(16));
+    myMap.assign(17, new Foo(17));
+    Foo* foo14 = myMap.get(14);
+    Foo* foo15 = myMap.get(15);
+    Foo* foo16 = myMap.get(16);
+    Foo* foo17 = myMap.get(17);
+    std::cout << "Value for key 14 is " << foo14->a << std::endl;
+    std::cout << "Value for key 15 is " << foo15->a << std::endl;
+    std::cout << "Value for key 16 is " << foo16->a << std::endl;
+    std::cout << "Value for key 17 is " << foo17->a << std::endl;
 }
 
 int main() {
